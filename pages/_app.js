9276019025d41acb83da7dc35304659a84b4cb6d
congtrung6391 <congtrung6391@gmail.com>
2022-12-18 16:54:36 +0700

    override mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
-import theme from '@/lib/mui/theme';
+import ThemeConfig from '../lib/mui';
 import createEmotionCache from 'modules/share/utils/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -18,11 +16,9 @@ export default function MyApp(props) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
+      <ThemeConfig>
         <Component {...pageProps} />
-      </ThemeProvider>
+      </ThemeConfig>
     </CacheProvider>
   );
 }
