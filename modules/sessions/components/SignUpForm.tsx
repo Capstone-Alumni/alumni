d4113c80b85bc1896364f8334ddf9ded009c2eff
congtrung6391 <congtrung6391@gmail.com>
2022-12-23 02:02:41 +0700

    add validation

--- modules/sessions/components/SignUpForm.tsx
+++ modules/sessions/components/SignUpForm.tsx
@@ -3,18 +3,20 @@
 import noop from 'lodash/fp/noop';
 import omit from 'lodash/fp/omit';
 
+import * as yup from 'yup';
+
 import { useRouter } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 import { Controller, useForm } from 'react-hook-form';
 import {
   Box,
-  Typography,
   Button,
   Divider,
   IconButton,
   Link,
   Stack,
   TextField,
+  Typography,
 } from '@mui/material';
 
 import googleFill from '@iconify/icons-eva/google-fill';
@@ -22,18 +24,59 @@ import twitterFill from '@iconify/icons-eva/twitter-fill';
 import facebookFill from '@iconify/icons-eva/facebook-fill';
 import { Icon } from '@iconify/react';
 
+import useYupValidateionResolver from 'modules/share/utils/useYupValidationResolver';
+
 type FormValues = {
   fullName: string;
   username: string;
   email: string;
   password: string;
   confirmPassword: string;
-}
+};
+
+const validationSchema = yup
+  .object({
+    fullName: yup
+      .string()
+      .matches(
+        /^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/,
+        'Tên của bạn không hợp lệ',
+      )
+      .max(40, 'Tối đa 40 ký tự')
+      .required('Bắt buộc'),
+    username: yup
+      .string()
+      .matches(
+        /^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._@]+(?<![_.])$/, // Không bắt đàu bằng . hoặc _, không chứa ._ _. __ .. ở giữa, không kêt thúc bằng _ hoặc ., chỉ chứa a-zA-Z0-9._@
+        'Tên tài khoản không hợp lệ',
+      )
+      .min(6, 'Tổi thiểu 6 ký tự')
+      .max(40, 'Tối đa 40 ký tự')
+      .required('Bắt buộc'),
+    email: yup.string().email('Email không hợp lệ').required('Bắt buộc'),
+    password: yup
+      .string()
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        'Mật khẩu phải chứa ít nhất 8 ký tự, trong đó có phải có 1 chữ in thường, 1 chữ in hoa, 1 chữ số, và 1 ký tự đặt biệt',
+      )
+      .min(8, 'Tối thiểu 8 ký tự')
+      .max(40, 'Tối đa 40 ký tự')
+      .required('Bắt buộc'),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), null], 'Mật khẩu không khớp')
+      .required('Bắt buộc'),
+  })
+  .required();
 
 const SignUpForm = () => {
   const router = useRouter();
 
-  const { control, handleSubmit } = useForm<FormValues>({
+  const resolver = useYupValidateionResolver(validationSchema);
+
+  const { control, handleSubmit, getValues } = useForm<FormValues>({
+    mode: 'onChange',
     defaultValues: {
       fullName: '',
       username: '',
@@ -41,12 +84,13 @@ const SignUpForm = () => {
       password: '',
       confirmPassword: '',
     },
+    resolver,
   });
 
   const onSubmit = (values: FormValues) => {
     const payload = omit(['comfirmPassword'])(values);
     router.push('/verify-account');
-  }
+  };
 
   return (
     <Box
@@ -70,18 +114,22 @@ const SignUpForm = () => {
           <Typography sx={{ mr: 1 }} variant="body2">
             Đã có tài khoản?
           </Typography>
-          <Link variant="subtitle2" href="/sign_in">Đăng nhập tại đây</Link>
+          <Link variant="subtitle2" href="/sign_in">
+            Đăng nhập tại đây
+          </Link>
         </Box>
 
         <Controller
           control={control}
           name="fullName"
-          render={({ field }) => (
+          render={({ field, fieldState: { error } }) => (
             <TextField
               fullWidth
-              label="Họ và tên"
+              label="Họ và tên *"
               {...field}
-              sx={{ mb: 3 }}  
+              error={Boolean(error?.message)}
+              helperText={error?.message}
+              sx={{ mb: 3 }}
             />
           )}
         />
@@ -89,12 +137,14 @@ const SignUpForm = () => {
         <Controller
           control={control}
           name="username"
-          render={({ field }) => (
+          render={({ field, fieldState: { error } }) => (
             <TextField
               fullWidth
-              label="Tên tài khoản"
+              label="Tên tài khoản *"
               {...field}
-              sx={{ mb: 3 }}  
+              error={Boolean(error?.message)}
+              helperText={error?.message}
+              sx={{ mb: 3 }}
             />
           )}
         />
@@ -102,12 +152,14 @@ const SignUpForm = () => {
         <Controller
           control={control}
           name="email"
-          render={({ field }) => (
+          render={({ field, fieldState: { error } }) => (
             <TextField
               fullWidth
-              label="Email"
+              label="Email *"
               {...field}
-              sx={{ mb: 3 }}  
+              error={Boolean(error?.message)}
+              helperText={error?.message}
+              sx={{ mb: 3 }}
             />
           )}
         />
@@ -115,12 +167,15 @@ const SignUpForm = () => {
         <Controller
           control={control}
           name="password"
-          render={({ field }) => (
+          render={({ field, fieldState: { error } }) => (
             <TextField
               fullWidth
-              label="Mật khẩu"
+              label="Mật khẩu *"
               {...field}
-              sx={{ mb: 3 }}  
+              type="password"
+              error={Boolean(error?.message)}
+              helperText={error?.message}
+              sx={{ mb: 3 }}
             />
           )}
         />
@@ -128,17 +183,25 @@ const SignUpForm = () => {
         <Controller
           control={control}
           name="confirmPassword"
-          render={({ field }) => (
+          render={({ field, fieldState: { error } }) => (
             <TextField
               fullWidth
-              label="Xác thực mật khẩu"
+              label="Xác thực mật khẩu *"
+              type="password"
               {...field}
-              sx={{ mb: 3 }}  
+              error={Boolean(error?.message)}
+              helperText={error?.message}
+              sx={{ mb: 3 }}
             />
           )}
         />
 
-        <Button fullWidth size="large" variant="contained" onClick={handleSubmit(onSubmit)}>
+        <Button
+          fullWidth
+          size="large"
+          variant="contained"
+          onClick={handleSubmit(onSubmit)}
+        >
           Đăng ký
         </Button>
 
