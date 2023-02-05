2d2060b32608fd7c611d48635705a2d779513beb
congtrung6391 <congtrung6391@gmail.com>
2023-02-06 03:41:24 +0700

    refactor

--- prisma/migrations/20230205201046_re_add_publicity_career_education_full_name/migration.sql
+++ prisma/migrations/20230205201046_re_add_publicity_career_education_full_name/migration.sql
@@ -1,8 +1,8 @@
 -- AlterTable
 SELECT migrate_schema(
     '
-      ALTER TABLE "informations" ADD COLUMN     "career_publicity" "ScopePublicity" NOT NULL,
-      ADD COLUMN     "education_publicity" "ScopePublicity" NOT NULL,
+      ALTER TABLE "informations" ADD COLUMN     "career_publicity" "ScopePublicity" NOT NULL DEFAULT ''PRIVATE'',
+      ADD COLUMN     "education_publicity" "ScopePublicity" NOT NULL DEFAULT ''PRIVATE'',
       ADD COLUMN     "fullName" TEXT;
     '
 )
