2075ef26ffd4142181c306bcaa4ef65c7a0fc15b
congtrung6391 <congtrung6391@gmail.com>
2023-02-09 10:49:26 +0700

    remove redundant code

--- src/app/(admin_layout)/admin/access_request/page.tsx
+++ src/app/(admin_layout)/admin/access_request/page.tsx
@@ -1,13 +1,3 @@
-import { unstable_getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
-import { nextAuthOptions } from 'src/pages/api/auth/[...nextauth]';
-
 export default async function Page() {
-  const session = await unstable_getServerSession(nextAuthOptions);
-
-  if (!session) {
-    redirect('/');
-  }
-
   return <div>Coming soon</div>;
 }
--- src/app/(admin_layout)/admin/members/page.tsx
+++ src/app/(admin_layout)/admin/members/page.tsx
@@ -1,14 +1,5 @@
-import { unstable_getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
 import MemberListPage from 'src/modules/members/components/MemberListPage';
-import { nextAuthOptions } from 'src/pages/api/auth/[...nextauth]';
 
 export default async function Page() {
-  const session = await unstable_getServerSession(nextAuthOptions);
-
-  if (!session) {
-    redirect('/');
-  }
-
   return <MemberListPage />;
 }
