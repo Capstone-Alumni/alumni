069c00259c5c6233d4934ab5892ba64980536f6c
pvnamGit <pvnam.forwork@gmail.com>
2023-02-15 15:16:28 +0700

    api crud comment news

---
+++ src/modules/news/controller/newsComment.controller.ts
@@ -0,0 +1,109 @@
+import { NextApiRequestWithTenant } from '@lib/next-connect';
+import getPrismaClient from '@lib/prisma/prisma';
+import { NextApiResponse } from 'next';
+import { ApiErrorResponse, ApiSuccessResponse } from 'src/types';
+import NewsCommentService from '../services/newsComment.service';
+
+export default class NewsCommentController {
+  static create = async (
+    req: NextApiRequestWithTenant,
+    res: NextApiResponse<ApiSuccessResponse | ApiErrorResponse>,
+  ) => {
+    try {
+      const prisma = await getPrismaClient(req.tenantId);
+      const { id: newsId } = req.query;
+      const userId = req.user.id;
+      const newsCreated = await NewsCommentService.create(
+        prisma,
+        newsId as string,
+        userId as string,
+        req.body,
+      );
+      return res.status(201).json({
+        status: true,
+        data: newsCreated,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  };
+
+  static getList = async (
+    req: NextApiRequestWithTenant,
+    res: NextApiResponse<ApiSuccessResponse | ApiErrorResponse>,
+  ) => {
+    try {
+      const prisma = await getPrismaClient(req.tenantId);
+      const { id: newsId } = req.query;
+      const newsComments = await NewsCommentService.getList(
+        prisma,
+        newsId as string,
+        req.query,
+      );
+      return res.status(200).json({
+        status: true,
+        data: newsComments,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  };
+
+  static updateComment = async (
+    req: NextApiRequestWithTenant,
+    res: NextApiResponse<ApiSuccessResponse | ApiErrorResponse>,
+  ) => {
+    try {
+      const prisma = await getPrismaClient(req.tenantId);
+      const { commentId } = req.query;
+      const userId = req.user.id;
+
+      const newsCommentUpdated = await NewsCommentService.update(
+        prisma,
+        commentId as string,
+        userId,
+        req.body,
+      );
+      return res.status(200).json({
+        status: true,
+        data: newsCommentUpdated,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  };
+
+  static deleteComment = async (
+    req: NextApiRequestWithTenant,
+    res: NextApiResponse<ApiSuccessResponse | ApiErrorResponse>,
+  ) => {
+    try {
+      const prisma = await getPrismaClient(req.tenantId);
+      const { commentId } = req.query;
+      const userId = req.user.id;
+      const newsCommentDeleted = await NewsCommentService.delete(
+        prisma,
+        commentId as string,
+        userId,
+      );
+      return res.status(200).json({
+        status: true,
+        data: newsCommentDeleted,
+      });
+    } catch (error: any) {
+      return res.status(400).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  };
+}
---
+++ src/modules/news/services/newsComment.service.ts
@@ -0,0 +1,117 @@
+import { PrismaClient } from '@prisma/client';
+import { getPageAndLimitFromParams } from 'src/utils';
+
+export default class NewsCommentService {
+  static create = async (
+    tenantPrisma: PrismaClient,
+    newsId: string,
+    userId: string,
+    body: any,
+  ) => {
+    const information = await tenantPrisma.information.findFirst({
+      where: { userId: userId },
+    });
+    const newsCommetCreated = await tenantPrisma.newsComment.create({
+      data: {
+        ...body,
+        commenterId: userId,
+        news: {
+          connect: { id: newsId },
+        },
+        commenterInfo: {
+          connect: { id: information!.id },
+        },
+      },
+    });
+    return newsCommetCreated;
+  };
+
+  static getList = async (
+    tenantPrisma: PrismaClient,
+    newsId: string,
+    params: any,
+  ) => {
+    const { page, limit } = getPageAndLimitFromParams(params);
+    const whereFilter = {
+      AND: [{ newsId: newsId }, { archived: false }],
+    };
+    const [totalNewsComments, newsComments] = await tenantPrisma.$transaction([
+      tenantPrisma.newsComment.count({
+        where: whereFilter,
+      }),
+      tenantPrisma.newsComment.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: whereFilter,
+        include: {
+          commenterInfo: {
+            select: {
+              id: true,
+              fullName: true,
+              avatarUrl: true,
+            },
+          },
+        },
+        orderBy: [
+          {
+            createdAt: 'desc',
+          },
+        ],
+      }),
+    ]);
+    return {
+      totalItems: totalNewsComments,
+      items: newsComments,
+      itemPerPage: limit,
+    };
+  };
+
+  static update = async (
+    tenantPrisma: PrismaClient,
+    commentId: string,
+    userId: string,
+    body: any,
+  ) => {
+    const newsComment = await tenantPrisma.newsComment.findFirst({
+      where: { id: commentId, archived: false },
+    });
+
+    if (!newsComment) {
+      throw new Error('Comment is not existed');
+    } else if (newsComment && newsComment.commenterId !== userId) {
+      throw new Error('Not allowed to update this comment');
+    }
+
+    const newsCommetUpdated = await tenantPrisma.newsComment.update({
+      where: { id: commentId },
+      data: {
+        ...body,
+      },
+    });
+    return newsCommetUpdated;
+  };
+
+  static delete = async (
+    tenantPrisma: PrismaClient,
+    commentId: string,
+    userId: string,
+  ) => {
+    const newsComment = await tenantPrisma.newsComment.findFirst({
+      where: { id: commentId, archived: false },
+    });
+
+    if (!newsComment) {
+      throw new Error('Comment is not existed');
+    } else if (newsComment && newsComment.commenterId !== userId) {
+      throw new Error('Not allowed to delete this comment');
+    }
+
+    const newsCommetDeleted = await tenantPrisma.newsComment.update({
+      where: { id: commentId },
+      data: {
+        archived: true,
+      },
+    });
+    return newsCommetDeleted;
+  };
+}
