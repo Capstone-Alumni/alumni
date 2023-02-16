dd5edf49b6962b4ee09ced3867dab63ca3d5baa3
Hoàng Các Loan Châu <hoangchau741@gmail.com>
2023-02-17 00:20:37 +0700

    recruitment service

---
+++ src/modules/recruitments/services/recruitments.service.ts
@@ -0,0 +1,179 @@
+import { PrismaClient } from '@prisma/client';
+import {
+  CreateRecruitmentProps,
+  GetListRecruitmentParams,
+  UpdateRecruitmentProps,
+} from '../type';
+import { getPageAndLimitFromParams } from '../../../utils';
+
+const isRecruitmentExisted = async (
+  tenantPrisma: PrismaClient,
+  recruitmentId: string,
+) => {
+  const recruitment = await tenantPrisma.recruitment.findFirst({
+    where: {
+      AND: [{ id: recruitmentId }, { archived: false }],
+    },
+  });
+  if (!recruitment) {
+    throw new Error('Recruitment is not Existed');
+  }
+};
+
+export default class RecruimentService {
+  static create = async (
+    tenantPrisma: PrismaClient,
+    recruitmentOwnerId: string,
+    body: CreateRecruitmentProps,
+  ) => {
+    const newRecruitment = await tenantPrisma.recruitment.create({
+      data: {
+        ...body,
+        recruitmentOwnerId: recruitmentOwnerId,
+      },
+    });
+    return newRecruitment;
+  };
+
+  static update = async (
+    tenantPrisma: PrismaClient,
+    recruitmentId: string,
+    body: UpdateRecruitmentProps,
+  ) => {
+    await isRecruitmentExisted(tenantPrisma, recruitmentId);
+    const updatedRecruitment = await tenantPrisma.recruitment.update({
+      where: { id: recruitmentId },
+      data: body,
+    });
+    return updatedRecruitment;
+  };
+
+  static delete = async (tenantPrisma: PrismaClient, recruitmentId: string) => {
+    await isRecruitmentExisted(tenantPrisma, recruitmentId);
+    const deletedRecruitment = await tenantPrisma.recruitment.update({
+      where: { id: recruitmentId },
+      data: {
+        archived: true,
+      },
+    });
+    return deletedRecruitment;
+  };
+
+  static getById = async (
+    tenantPrisma: PrismaClient,
+    recruitmentId: string,
+  ) => {
+    await isRecruitmentExisted(tenantPrisma, recruitmentId);
+    const recruitment = await tenantPrisma.recruitment.findFirst({
+      where: { id: recruitmentId },
+    });
+    return recruitment;
+  };
+
+  static getList = async (
+    tenantPrisma: PrismaClient,
+    params: GetListRecruitmentParams,
+  ) => {
+    const { page, limit } = getPageAndLimitFromParams(params);
+
+    const { companyName, job, position, salary, type } = params;
+    const whereFilter = {
+      AND: [
+        { companyName: { contains: companyName } },
+        { job: { contains: job } },
+        { position: { contains: position } },
+        { salary: { contains: salary } },
+        { type: { contains: type } },
+        { archived: false },
+      ],
+    };
+
+    const [totalItem, recruitmentItem] = await tenantPrisma.$transaction([
+      tenantPrisma.recruitment.count({
+        where: whereFilter,
+      }),
+      tenantPrisma.recruitment.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: whereFilter,
+        orderBy: [{ createdAt: 'desc' }],
+      }),
+    ]);
+    return {
+      totalItems: totalItem,
+      items: recruitmentItem,
+      itemPerPage: limit,
+    };
+  };
+
+  static getAprovedList = async (
+    tenantPrisma: PrismaClient,
+    params: GetListRecruitmentParams,
+  ) => {
+    const { page, limit } = getPageAndLimitFromParams(params);
+
+    const { companyName, job, position, salary, type } = params;
+    const whereFilter = {
+      AND: [
+        { companyName: { contains: companyName } },
+        { job: { contains: job } },
+        { position: { contains: position } },
+        { salary: { contains: salary } },
+        { type: { contains: type } },
+        { archived: false },
+        { isAproved: true },
+      ],
+    };
+
+    const [totalItem, recruitmentItem] = await tenantPrisma.$transaction([
+      tenantPrisma.recruitment.count({
+        where: whereFilter,
+      }),
+      tenantPrisma.recruitment.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: whereFilter,
+        orderBy: [{ createdAt: 'desc' }],
+      }),
+    ]);
+    return {
+      totalItems: totalItem,
+      items: recruitmentItem,
+      itemPerPage: limit,
+    };
+  };
+
+  static getApprovedDetailRecruitment = async (
+    tenantPrisma: PrismaClient,
+    recruitmentId: string,
+  ) => {
+    await isRecruitmentExisted(tenantPrisma, recruitmentId);
+    const recruitment = await tenantPrisma.recruitment.findFirst({
+      where: { id: recruitmentId, isApproved: true },
+    });
+    return recruitment;
+  };
+
+  static approve = async (
+    tenantPrisma: PrismaClient,
+    recruitmentId: string,
+  ) => {
+    const recruitment = await tenantPrisma.recruitment.update({
+      where: { id: recruitmentId },
+      data: {
+        isApproved: true,
+      },
+    });
+    return recruitment;
+  };
+
+  static reject = async (tenantPrisma: PrismaClient, recruitmentId: string) => {
+    const recruitment = await tenantPrisma.recruitment.update({
+      where: { id: recruitmentId },
+      data: {
+        isApproved: false,
+      },
+    });
+    return recruitment;
+  };
+}
---
+++ src/modules/recruitments/type.ts
@@ -0,0 +1,38 @@
+//BE
+export type CreateRecruitmentProps = {
+  companyName: string;
+  website: string;
+  job: string;
+  address: string;
+  position: string;
+  title: string;
+  description: string;
+  type: string; // parttime, full time, etc ..
+  salary: string;
+  startAt: string;
+  expiredAt: string;
+};
+
+export type UpdateRecruitmentProps = {
+  companyName?: string;
+  website?: string;
+  job?: string;
+  address?: string;
+  position?: string;
+  title?: string;
+  description?: string;
+  type?: string; // parttime, full time, etc ..
+  salary?: string;
+  startAt?: string;
+  expiredAt?: string;
+};
+
+export type GetListRecruitmentParams = {
+  page: number;
+  limit: number;
+  companyName?: string;
+  job?: string;
+  position?: string;
+  type?: string;
+  salary?: string;
+};
