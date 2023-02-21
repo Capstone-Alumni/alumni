7c334d7d8665896e5f4abb44ab7c5fc72bd57734
Hoàng Các Loan Châu <hoangchau741@gmail.com>
2023-02-21 16:19:21 +0700

    fix quote

--- prisma/migrations/20230221071305_add_recruitment_owner_info/migration.sql
+++ prisma/migrations/20230221071305_add_recruitment_owner_info/migration.sql
@@ -1,6 +1,8 @@
+SELECT template.run_migration('
 -- AlterTable
 ALTER TABLE "recruitments" ADD COLUMN     "recruitment_owner_info_id" TEXT,
 ALTER COLUMN "recruitment_owner_id" DROP NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE "recruitments" ADD CONSTRAINT "recruitments_recruitment_owner_info_id_fkey" FOREIGN KEY ("recruitment_owner_info_id") REFERENCES "informations"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+');
