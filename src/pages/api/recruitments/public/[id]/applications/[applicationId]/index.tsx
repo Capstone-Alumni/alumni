1954b2c5e8ceb076fde69697277468488c9bcdea
Hoàng Các Loan Châu <hoangchau741@gmail.com>
2023-03-01 22:20:20 +0700

    revert

--- src/pages/api/recruitments/public/[id]/applications/[applicationId]/index.tsx
+++ src/pages/api/recruitments/public/[id]/applications/[applicationId]/index.tsx
@@ -2,9 +2,8 @@ import { extractTenantId } from '@lib/next-connect';
 import { isAuthenticatedUser } from '@lib/next-connect/apiMiddleware';
 import onErrorAPIHandler from '@lib/next-connect/onErrorAPIHandler';
 import onNoMatchAPIHandler from '@lib/next-connect/onNoMatchAPIHandler';
-import ApplicationController from 'src/modules/recruitments/controllers/recruitmentApplication.controller';
 import nc from 'next-connect';
-
+import ApplicationController from 'src/modules/recruitments/controllers/recruitmentApplication.controller';
 const handler = nc({
   onError: onErrorAPIHandler,
   onNoMatch: onNoMatchAPIHandler,
