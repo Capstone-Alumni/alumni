68f2961ea19d600ee4efbfef6a3708e03ae6e296
congtrung6391 <congtrung6391@gmail.com>
2023-03-03 23:55:17 +0700

    remove counter slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './slices/counterSlice';
 import currentUserReducer from './slices/currentUserSlice';
-// import currentTenantReducer from './slices/currentTenantSlice';
 import { userProfileApi } from './slices/userProfileSlice';
 import { searchProfilesApi } from './slices/searchProfiles';
 import { newsSliceApi } from './slices/newsSlice';
@@ -9,8 +7,6 @@ import { newsCommentSliceApi } from './slices/newsCommentSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    // currentTenant: currentTenantReducer,
     currentUser: currentUserReducer,
     [userProfileApi.reducerPath]: userProfileApi.reducer,
     [newsSliceApi.reducerPath]: newsSliceApi.reducer,
