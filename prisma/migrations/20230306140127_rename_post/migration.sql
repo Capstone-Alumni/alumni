c156fc16ad54660aa20067b6861434e3c21b9d6d
congtrung6391 <congtrung6391@gmail.com>
2023-03-06 21:03:32 +0700

    rename post

---
+++ prisma/migrations/20230306140127_rename_post/migration.sql
@@ -0,0 +1,41 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+SELECT template.run_migration('
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_author_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "post_comments" DROP CONSTRAINT "post_comments_post_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "post_likes" DROP CONSTRAINT "post_likes_post_id_fkey";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" TEXT NOT NULL,
+    "archived" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT NOT NULL,
+    "publicity" "AccessLevel" NOT NULL DEFAULT ''ALUMNI'',
+    "author_id" TEXT NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "informations"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_comments" ADD CONSTRAINT "post_comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "post_likes" ADD CONSTRAINT "post_likes_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+');
