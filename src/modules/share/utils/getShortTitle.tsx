06faf3acea8a6918a2a7b147ffb9dbe6d60805f2
congtrung6391 <congtrung6391@gmail.com>
2023-03-07 00:59:51 +0700

    shorten title

--- src/modules/share/utils/getShortTitle.tsx
+++ src/modules/share/utils/getShortTitle.tsx
@@ -1,8 +1,8 @@
 import { Tooltip } from '@mui/material';
 
 export const getShortTitle = (title: string) => {
-  if (title.length > 60) {
-    const shortTitle = `${title.substring(0, 60)}...`;
+  if (title.length > 30) {
+    const shortTitle = `${title.substring(0, 30)}...`;
     return (
       <Tooltip title={title}>
         <>{shortTitle}</>
