d57082fb79d2a2531a9ac9737b0201881dd9599e
congtrung6391 <congtrung6391@gmail.com>
2023-03-26 15:20:14 +0700

    refactor route

--- src/app/(admin_layout)/layout.tsx
+++ src/app/(admin_layout)/layout.tsx
@@ -1,22 +1,22 @@
 import AdminNav from '@share/components/layout/AdminNav';
 import AdminLayoutWrapper from '@share/components/layout/AdminLayoutWrapper';
 import AdminBodyWrapper from '@share/components/layout/AdminBodyWrapper';
-import {
-  getTenantDataSSR,
-  verifyAdminOrMod,
-} from '@share/helpers/SSRAuthorization';
+import { getTenantDataSSR } from '@share/helpers/SSRAuthorization';
+import { getServerSession } from 'next-auth';
 
 export default async function AuthorizedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await verifyAdminOrMod();
+  const session = await getServerSession();
   const data = await getTenantDataSSR();
+
+  console.log(session);
 
   return (
     <AdminLayoutWrapper>
-      <AdminNav user={session.user} tenant={data} />
+      <AdminNav user={session?.user} tenant={data} />
       <AdminBodyWrapper>{children}</AdminBodyWrapper>
     </AdminLayoutWrapper>
   );
