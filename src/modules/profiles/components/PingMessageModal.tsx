7ebb6a0ac7cd6b97176b1f5fee7443f376c9e4a8
Gia An <buigiaanfb1@gmail.com>
2023-04-02 02:31:53 +0700

    style: uplife styles

--- src/modules/profiles/components/PingMessageModal.tsx
+++ src/modules/profiles/components/PingMessageModal.tsx
@@ -13,6 +13,8 @@ import {
   DialogTitle,
   Stack,
   TextField,
+  Typography,
+  styled,
 } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import useSendMessage from '../hooks/useSendMessage';
@@ -27,9 +29,35 @@ interface PingMessageModalProps {
 const messageSchema = Yup.object().shape({
   message: Yup.string()
     .required('Tin nhắn không được để trống')
-    .min(1, 'Must be exactly 5 digits')
-    .max(100, 'Must be exactly 5 digits'),
+    .min(1)
+    .max(100, 'Tối đa 100 kí tự'),
 });
+
+const StyledDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiPaper-root': {
+    padding: theme.spacing(0),
+    width: '700px',
+    margin: '0 auto',
+    maxWidth: '100vw',
+  },
+}));
+
+const StyledDivider = styled('div')(({ theme }) => ({
+  display: 'flex',
+}));
+
+const StyledWrapperImageCover = styled('div')(({ theme }) => ({
+  width: '45%',
+  '& img': {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  },
+}));
+
+const StyledWrapperContent = styled('div')(({ theme }) => ({
+  width: '55%',
+}));
 
 export default function PingMessageModal({
   children,
@@ -80,58 +108,69 @@ export default function PingMessageModal({
         {children}
       </div>
       <div>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          sx={{ width: '700px', margin: '0 auto' }}
-        >
-          <FormikProvider value={formik}>
-            <Form noValidate autoComplete="off" onSubmit={handleSubmit}>
-              <DialogTitle>Người bạn quen?</DialogTitle>
-              <DialogContent>
-                <Stack sx={{ py: 1 }}>
-                  <DialogContentText>
-                    Bằng cách gửi tin nhắn thông qua SMS (tối đa 100 kí tự) tới
-                    người này và bạn được gửi tối đa 1 lần
-                  </DialogContentText>
-                </Stack>
-                <Box>
-                  <Stack sx={{ py: '0.25rem' }}>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        flexDirection: 'row',
-                        gap: '1rem',
-                      }}
-                    >
-                      <TextField
-                        label="Tin nhắn"
-                        multiline
-                        fullWidth
-                        {...getFieldProps('message')}
-                        error={Boolean(touched.message && errors.message)}
-                        helperText={touched.message && errors.message}
-                      />
+        <StyledDialog open={open} onClose={handleClose}>
+          <StyledDivider>
+            <StyledWrapperImageCover>
+              <img src="https://www.kindpng.com/picc/m/243-2436343_boy-best-friends-cartoon-friends-png-transparent-png.png" />
+            </StyledWrapperImageCover>
+            <StyledWrapperContent>
+              <FormikProvider value={formik}>
+                <Form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                  <DialogContent sx={{ paddingBottom: '0' }}>
+                    <Typography variant="h3">Người bạn biết?</Typography>
+                    <Stack sx={{ margin: '0.5rem 0' }}>
+                      <Typography variant="body2" color={'red'}>
+                        Lưu ý*: Bằng cách gửi tin nhắn thông qua SMS (tối đa 100
+                        kí tự) tới người này và bạn được gửi tối đa 1 lần.
+                      </Typography>
+                    </Stack>
+                    <Box>
+                      <Stack sx={{ py: '0.5rem' }}>
+                        <Box
+                          sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: '1rem',
+                          }}
+                        >
+                          <TextField
+                            label="Tin nhắn"
+                            rows={3}
+                            multiline
+                            fullWidth
+                            {...getFieldProps('message')}
+                            error={Boolean(touched.message && errors.message)}
+                            helperText={touched.message && errors.message}
+                          />
+                          <div>
+                            <Typography variant="body2">
+                              Kí tự còn lại:{' '}
+                              {100 - getFieldProps('message').value.length < 0
+                                ? 0
+                                : 100 - getFieldProps('message').value.length}
+                            </Typography>
+                          </div>
+                        </Box>
+                      </Stack>
                     </Box>
-                  </Stack>
-                </Box>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose} color="inherit">
-                  Huỷ
-                </Button>
-                <LoadingButton
-                  type="submit"
-                  variant="contained"
-                  loading={isSubmitting}
-                >
-                  Gửi
-                </LoadingButton>
-              </DialogActions>
-            </Form>
-          </FormikProvider>
-        </Dialog>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose} color="inherit">
+                      Huỷ
+                    </Button>
+                    <LoadingButton
+                      type="submit"
+                      variant="contained"
+                      loading={isSubmitting}
+                    >
+                      Gửi
+                    </LoadingButton>
+                  </DialogActions>
+                </Form>
+              </FormikProvider>
+            </StyledWrapperContent>
+          </StyledDivider>
+        </StyledDialog>
       </div>
     </>
   );
