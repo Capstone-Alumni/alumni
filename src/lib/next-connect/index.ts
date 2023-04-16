5870745c30f7871d948cfdf68e6dc1f68441b58a
congtrung6391 <congtrung6391@gmail.com>
2023-04-16 22:08:13 +0700

    fix redux news API missing tenantId

--- src/lib/next-connect/index.ts
+++ src/lib/next-connect/index.ts
@@ -2,6 +2,8 @@ import nc, { NextHandler } from 'next-connect';
 import { NextApiRequest, NextApiResponse } from 'next';
 import onErrorAPIHandler from './onErrorAPIHandler';
 import onNoMatchAPIHandler from './onNoMatchAPIHandler';
+import { getServerSession } from 'next-auth';
+import { nextAuthOptions } from 'src/pages/api/auth/[...nextauth]';
 
 export type NextApiRequestWithTenant = {
   tenantId: string;
@@ -13,6 +15,24 @@ export const extractTenantId = async (
   next: NextHandler,
 ) => {
   const tenantId = req.headers['tenant-id'] as string;
+
+  if (!tenantId) {
+    throw new Error('unauthorized');
+  }
+
+  req.tenantId = tenantId;
+
+  next();
+};
+
+export const extractTenantIdFromSession = async (
+  req: NextApiRequestWithTenant,
+  res: NextApiResponse,
+  next: NextHandler,
+) => {
+  const session = await getServerSession(req, res, nextAuthOptions);
+  console.log(session);
+  const tenantId = session?.currentTenant?.tenantId;
 
   if (!tenantId) {
     throw new Error('unauthorized');
--- src/pages/api/news/[id]/index.ts
+++ src/pages/api/news/[id]/index.ts
@@ -1,6 +1,6 @@
 import { verifySchoolAdmin } from '@lib/next-connect/apiMiddleware';
 import NewsController from 'src/modules/news/controller/news.controller';
-import { extractTenantId } from '@lib/next-connect';
+import { extractTenantIdFromSession } from '@lib/next-connect';
 import onErrorAPIHandler from '@lib/next-connect/onErrorAPIHandler';
 import onNoMatchAPIHandler from '@lib/next-connect/onNoMatchAPIHandler';
 import nc from 'next-connect';
@@ -8,7 +8,7 @@ import nc from 'next-connect';
 const handler = nc({
   onError: onErrorAPIHandler,
   onNoMatch: onNoMatchAPIHandler,
-}).use(extractTenantId);
+}).use(extractTenantIdFromSession);
 
 handler
   .get(verifySchoolAdmin, NewsController.getNewsById)
