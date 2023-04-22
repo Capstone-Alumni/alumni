055a0133f8b4f0ffdccf43b464b21509825141bf
congtrung6391 <congtrung6391@gmail.com>
2023-04-23 02:03:43 +0700

    fix build

--- src/modules/verifyAccount/states.ts
+++ src/modules/verifyAccount/states.ts
@@ -3,7 +3,7 @@ import { atom } from 'recoil';
 import { Class, Grade } from '../gradeAndClass/types';
 
 export const selectedGradeAtom = atom<Grade | null>({
-  key: 'selectedGradeAtom',
+  key: 'selectedGradeVerifyAccountAtom',
   default: null,
 });
 
